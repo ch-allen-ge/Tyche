33066571b590585f013fdf6cd0cfa99d1ff7e266
Allen <d4rkmage80@gmail.com>
2024-02-02 20:32:50 -0500

    changed user to username

--- src/configs/db.config.js
+++ src/configs/db.config.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 const db = new Pool({
   host: process.env.PG_HOST,
   port: process.env.PG_PORT,
-  user: process.env.PG_USER,
+  user: process.env.PG_USERNAME,
   password: process.env.PG_PASSWORD,
   database: process.env.PG_DATABASE
 });
