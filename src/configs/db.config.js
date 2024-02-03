533d4956526bc71c00130088389d448637c38096
Allen <d4rkmage80@gmail.com>
2024-02-02 20:41:25 -0500

    making it work

--- src/configs/db.config.js
+++ src/configs/db.config.js
@@ -5,7 +5,10 @@ const db = new Pool({
   port: process.env.PG_PORT,
   user: process.env.PG_USERNAME,
   password: process.env.PG_PASSWORD,
-  database: process.env.PG_DATABASE
+  database: process.env.PG_DATABASE,
+  ssl: {
+    rejectUnauthorized: false,
+  }
 });
 
 module.exports = db;
