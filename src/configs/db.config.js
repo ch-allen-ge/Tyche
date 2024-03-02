e95eabe0d76e76c243ee88eb8c7289f1fba8ef3a
Allen Tan <d4rkmage80@gmail.com>
2024-03-02 18:18:30 -0500

    changing connection from details to conn string

--- src/configs/db.config.js
+++ src/configs/db.config.js
@@ -1,11 +1,7 @@
 const { Pool } = require('pg');
 
 const dbPool = {
-  host: process.env.PG_HOST,
-  port: process.env.PG_PORT,
-  user: process.env.PG_USERNAME,
-  password: process.env.PG_PASSWORD,
-  database: process.env.PG_DATABASE
+  connectionString: process.env.DATABASE_URL
 };
 
 if (process.env.NODE_ENV === 'production') {
